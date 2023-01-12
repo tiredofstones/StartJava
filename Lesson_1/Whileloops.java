0efa18198051800adde3a230eb1868beec1ef0bf
Suslov Denis <tiredofstones@gmail.com>
2023-01-12 21:05:36 +0900

    results While

---
+++ Lesson_1/Whileloops.java
@@ -0,0 +1,9 @@
+public class Whileloops {   
+    public static void main(String[] args) {
+        int value  = 0;
+        while(value<15){
+            System.out.println("Hello" +value);
+            value = value+1;
+        }
+    }
+}        
