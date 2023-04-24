aa1b2214fac2ab79dfc33c88c011f6bf537cf29b
Suslov Denis <tiredofstones@gmail.com>
2023-04-24 13:01:24 +0900

    Выполнил ДЗ №3

--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,27 +1,70 @@
 public class Wolf {
-    String sex;
-    String nickname;
-    float weight;
-    int age;
-    String color;
+    private String sex;
+    private String nickname;
+    private float weight;
+    private int age;
+    private String color;
 
-    void move() {
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getNickName() {
+        return nickname;
+    }
+    public void setNickName(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age > 8) {
+            System.out.println("Некорректный возраст");
+        } else {
+            this.age = age;
+        }
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void move() {
         System.out.println("Идет");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("Сидит");
     }  
 
-    void run() {
+    public void run() {
         System.out.println("Бежит");
     }  
 
-    void howl() {
+    public void howl() {
         System.out.println("Воет");
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("Охотится");
     }
 }
--- Lesson_2/WolfTest.java
+++ Lesson_2/WolfTest.java
@@ -1,13 +1,14 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf direWolf = new Wolf();
-        direWolf.sex = "Самец";
-        direWolf.nickname = "Призрак";
-        direWolf.weight = 80;
-        direWolf.age = 5;
-        direWolf.color = "Белый";
-        System.out.println("Пол - " + direWolf.sex + "\nКличка - " + direWolf.nickname + "\nВес - "
-                + direWolf.weight + "\nВозраст - " + direWolf.age + "\nОкрас - " + direWolf.color);
+        direWolf.setSex("Самец");
+        direWolf.setNickName("Призрак");
+        direWolf.setWeight(80);
+        direWolf.setAge(9);
+        direWolf.setColor("Белый");
+        System.out.println("Пол - " + direWolf.getSex() + "\nКличка - " + direWolf.getNickName() + 
+                "\nВес - " + direWolf.getWeight() + "\nВозраст - " + direWolf.getAge() + 
+                        "\nОкрас - " + direWolf.getColor());
 
         direWolf.move();
         direWolf.sit();
